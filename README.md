958d2252754d12676c0f791933c84b8fabedb728
AlexLacour <alexandre.lacour@xxii.fr>
2024-10-14 17:55:30 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # advent-of-code-monorepo-rust
+```bash
+cargo run --bin <the name of the day/file we want to solve>
+```
---
+++ aoc-2017/src/bin/day0.rs
@@ -0,0 +1,6 @@
+use aoc_utils;
+
+fn main() {
+    let x = aoc_utils::add(1, 1);
+    println!("Hello, world (from day 0-{x})!");
+}
